1036d502b5e77643063b4e234753d21d9b711f9b
Edward Villegas <cosmoscalibur@gmail.com>
2018-02-06 23:56:28 -0500

    Se incorpora toda la información del sistema planetario en info_programa.

--- info_programa.py
+++ info_programa.py
@@ -5,6 +5,6 @@ Created on Sun Jan 21 19:27:29 2018
 @author: USUARIO
 """
 
-import listas_planetas # Importa el archivo lista_plaentas que contiene los datos de las listas del archivo
-planetas=[listas_planetas.Me,listas_planetas.Ve,listas_planetas.Ti,listas_planetas.Ma,listas_planetas.Ju,listas_planetas.Sa,listas_planetas.Ur,listas_planetas.Ne]#se hace una lista de listas solo de planetas 
-estrelllas=333.31
+
+planetas=[[0.2, 1], [0.5, 5], [0.1, 8]]
+estrella=[5700, 1]
